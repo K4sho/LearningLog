f010d18e3f46e88b0210a7fd31be4d2dedaefe4f
kirill <kspavliy@gmail.com>
2019-10-15 16:01:19 +0300

    fix:Добился работоспособности, сделал страницу авторизации

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -3,6 +3,8 @@
 from django.conf.urls import url
 from . import views
 
+app_name = 'learning_logs'
+
 urlpatterns = [
     # Домашняя страница
     # TODO: Переделать на path
--- users/urls.py
+++ users/urls.py
@@ -1,13 +1,14 @@
 """Определяет схемы url для пользователей"""
 
 from django.conf.urls import url
-from django.contrib.auth import login, views as auth_views
+from django.contrib.auth import views as auth_views
 
 
 from . import views
 
+app_name = 'users'
+
 urlpatterns = [
     #  Страница входа
-    url(r'^login/$', login, auth_views.LoginView.as_view(template_name='users/login.html'),
-        name='login'),
+    url(r'^login/$', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
 ]
