1255694e4d049b0e929e0e27aaeb2c0dffe1649a
kasho <kspavliy@gmail.com>
2019-10-17 22:31:00 +0300

    add: Добавил вьюшку логина и logout. Нужно написать html

--- users/urls.py
+++ users/urls.py
@@ -11,4 +11,6 @@ app_name = 'users'
 urlpatterns = [
     #  Страница входа
     url(r'^login/$', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    url(r'^logout/$', views.logout_view, name='logout'),
+    url(r'^register/$', views.register, name='register'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.contrib.auth import logout, login, authenticate
+from django.urls import reverse
+from django.contrib.auth.forms import UserCreationForm
+
 
 # Create your views here.
+
+def logout_view(request):
+    """Завершает сеанс работы с приложением"""
+    logout(request)  # Вызываем библиотечную функцию logout
+    # и возвращаемся на домашнюю страницу
+    return HttpResponseRedirect(reverse("learning_logs:index"))
+
+
+def register(request):
+    if request.method != 'POST':
+        form = UserCreationForm()
+    else:
+        form = UserCreationForm(data=request.POST)
+
+        if form.is_valid():
+            new_user = form.save()  # Сохраняем созданный объекта юзера
+            # Выполнение входа и перенаправление на домашнюю страницу
+            authenticated_user = authenticate(username=new_user.username, password=request.POST['password1'])  #
+            # Возвращается проверенный объект пользователя
+            login(request, authenticated_user)  # Создается сессия с
+            # текущим пользователем
+            return HttpResponseRedirect(reverse("learning_logs:index"))
+    context = {'form': form}
+    return render(request, 'users/register.html', context)
