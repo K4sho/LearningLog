92b2eded1e26768dd17fd8f9e6bacf44fcfa5a58
kirill <kspavliy@gmail.com>
2019-11-12 09:57:21 +0300

    fix: fix settings

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -116,6 +116,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Мои настройки
@@ -141,7 +142,7 @@ if os.getcwd() == '/app':
     # Конфигурация статических ресурсов
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-    STATIC_ROOT = 'staticfiles'
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     STATIC_URL = '/static/'
     STATIC_TMP = os.path.join(BASE_DIR, 'static')
 
