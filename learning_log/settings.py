fe0d3fa8895c0dad3246e14f95f67cc529f6326e
kirill <kspavliy@gmail.com>
2019-11-12 11:06:07 +0300

    fix: import django_heroku

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -130,33 +131,4 @@ STATICFILES_DIRS = (
 # Django отправляет его на этот url адрес
 LOGIN_URL = '/users/login/'
 
-# Heroku's settings
-if os.getcwd() == '/app':
-    import dj_database_url
-    DATABASES = {
-        'default': dj_database_url.config(default='postgres://localhost')
-    }
-
-    # Поддержка заголовка 'X-Forwarded-Proto' для request.is_secure()
-    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-
-    DEBUG = False
-
-    # Хостом проекта может быть только heroku
-    ALLOWED_HOSTS = ['lelog-app.herokuapp.com']
-
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-
-    # Конфигурация статических ресурсов
-    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-    STATIC_URL = '/static/'
-    STATIC_TMP = os.path.join(BASE_DIR, 'static')
-
-    os.makedirs(STATIC_TMP, exist_ok=True)
-    os.makedirs(STATIC_ROOT, exist_ok=True)
-
-    # Extra places for collectstatic to find static files.
-    STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+django_heroku.settings(locals())
