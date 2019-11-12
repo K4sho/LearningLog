fcb391932a82db059077811315dd026f0ed8591c
kirill <kspavliy@gmail.com>
2019-11-12 10:25:57 +0300

    fix: fix settings

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -120,7 +120,10 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
 
 # Мои настройки
 # Когда пользователь не прошедший проверку запрашивает страницу, защищенную декоратором @login_required,
